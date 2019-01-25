03f9f5540dcec245f7c2f077a81c65d867f4fe14
Marks Namavirs <namavirs86@gmail.com>
2019-01-25 09:47:39 +0200

    Refactor loader

--- src/main/scala/com/gmail/namavirs86/app/Loader.scala
+++ src/main/scala/com/gmail/namavirs86/app/Loader.scala
@@ -3,13 +3,12 @@ package com.gmail.namavirs86.app
 import akka.actor.ActorSystem
 import com.gmail.namavirs86.app.Definitions.Games
 import com.gmail.namavirs86.game.blackjack
-import com.gmail.namavirs86.game.card.core.Game
 
 object Loader {
 
   def load(system: ActorSystem): Games = {
     Map(
-      blackjack.config.id → system.actorOf(Game.props(blackjack.config))
+      blackjack.id → system.actorOf(blackjack.props)
     )
   }
 
